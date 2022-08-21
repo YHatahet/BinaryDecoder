009e3c58b4fb3b3b04b738c977e70ba15c362beb
YHatahet <yamen.hatahet@gmail.com>
2022-08-22 00:07:14 +0400

    Added BinaryDecoder class with basic functions

---
+++ src/BinaryDecoder.js
@@ -0,0 +1,91 @@
+class BinaryDecoder {
+  /**
+   *
+   * @param {number[]} array
+   */
+  constructor(array) {
+    this.#registerSizeInBits = 8;
+    this.#init(array);
+  }
+
+  /**
+   * Initialize default private parameters and chained decoder class
+   * @param {number[]} array
+   */
+  #init(array) {
+    this.#result = {};
+    this.#endian = "big";
+    this.#bitIndex = 0;
+    this.binaryEquivalent = this.#arrToBinaryString(array);
+  }
+
+  /**
+   * Convert array to a binary string
+   * @param {Number[]} array
+   * @returns {String}
+   */
+  #arrToBinaryString(array) {
+    const outputArray = [];
+    for (const byte of array) {
+      const binaryEquivalent = this.#padZerosLeft(
+        byte.toString(2),
+        this.#registerSizeInBits
+      );
+      outputArray.push(binaryEquivalent);
+    }
+
+    return outputArray.join("");
+  }
+
+  /**
+   * Pad strings with 0s to the left
+   * @param {string} str
+   * @param {number} requiredLength
+   * @returns {string}
+   */
+  #padZerosLeft(str, requiredLength) {
+    if (typeof str !== "string")
+      throw new Error("padZeros: input not a string");
+    const stringLength = str.length;
+    if (stringLength >= requiredLength) return str;
+    return "0".repeat(requiredLength - stringLength) + str;
+  }
+
+  /**
+   * Fetches the result of the parsing process
+   * @returns {Object}
+   */
+  get result() {
+    return this.#result;
+  }
+
+  /**
+   * Decode the next amount of bits and give them a name. The decoded values will be stored in ".result"
+   * @param {Number} sizeInBits
+   * @param {String} name
+   * @param {Object} [options]
+   * @returns {this}
+   */
+  next(sizeInBits, name, options = {}) {
+    // Stop if there's no more data to parse
+    if (this.#bitIndex >= this.binaryEquivalent.length) return this;
+
+    const end = this.#bitIndex + sizeInBits;
+
+    const slice =
+      this.#endian === "little"
+        ? [...this.binaryEquivalent.slice(this.#bitIndex, end)]
+            .reverse()
+            .join("")
+        : this.binaryEquivalent.slice(this.#bitIndex, end);
+
+    let val = parseInt(slice, 2);
+
+    if (val !== undefined) this.#result[name] = val;
+
+    this.#bitIndex = end;
+    return this;
+  }
+}
+
+module.exports = BinaryDecoder;
