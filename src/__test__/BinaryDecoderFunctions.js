681219c9e587f24b6f3b2aed61c42a16f220d8ee
YHatahet <yamen.hatahet@gmail.com>
2022-08-22 22:18:23 +0400

    Added file for private functions

---
+++ src/__test__/BinaryDecoderFunctions.js
@@ -0,0 +1,19 @@
+module.exports = {
+  arrToBinaryString(array, registerSizeInBits = 8) {
+    const outputArray = [];
+    for (const byte of array) {
+      let binaryEquivalent = byte.toString(2);
+
+      const zerosToPad =
+        registerSizeInBits - binaryEquivalent.length > 0
+          ? registerSizeInBits - binaryEquivalent.length
+          : 0;
+
+      binaryEquivalent = "0".repeat(zerosToPad) + binaryEquivalent;
+
+      outputArray.push(binaryEquivalent);
+    }
+
+    return outputArray.join("");
+  },
+};
