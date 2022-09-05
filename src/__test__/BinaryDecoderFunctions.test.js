c15c2a910e011a97ff0ee0fc15df3ad5f06e09d2
YHatahet <yamen.hatahet@gmail.com>
2022-09-05 13:18:10 +0400

    Converted jest to zora

--- src/__test__/BinaryDecoderFunctions.test.js
+++ src/__test__/BinaryDecoderFunctions.test.js
@@ -2,23 +2,25 @@ const {
   arrToBinaryString,
   unsignedToSignedBits,
 } = require("./BinaryDecoderFunctions");
+const { test } = require("zora");
 
 // Testing unsigned to signed transformations
-test("8x 1's, follows by 16x 0's, and then 8x 1's", () => {
-  expect(arrToBinaryString([255, 0, 0, 255])).toBe(
+test("8x 1's, follows by 16x 0's, and then 8x 1's", (t) => {
+  t.equal(
+    arrToBinaryString([255, 0, 0, 255]),
     "11111111000000000000000011111111"
   );
 });
 
 // Testing unsigned to signed transformations
-test("Only sign should change", () => {
-  expect(unsignedToSignedBits(0b10000000, 8)).toBe(-1 * 0b10000000);
+test("Only sign should change", (t) => {
+  t.equal(unsignedToSignedBits(0b10000000, 8), -1 * 0b10000000);
 });
 
-test("No changes should occur", () => {
-  expect(unsignedToSignedBits(0b00000111, 8)).toBe(0b00000111);
+test("No changes should occur", (t) => {
+  t.equal(unsignedToSignedBits(0b00000111, 8), 0b00000111);
 });
 
-test("7 should give -1 when having a size of 3 bits", () => {
-  expect(unsignedToSignedBits(0b00000111, 3)).toBe(-1);
+test("7 should give -1 when having a size of 3 bits", (t) => {
+  t.equal(unsignedToSignedBits(0b00000111, 3), -1);
 });
